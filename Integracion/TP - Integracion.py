1051a5a7374daca16c03a610bf8fb77c149e6f06
Pablo <pablo.garay.dev@gmail.com>
2025-04-19 10:36:32 -0300

    Improved calculator and add docstring

--- Integracion/TP - Integracion.py
+++ Integracion/TP - Integracion.py
@@ -1,82 +1,154 @@
 # Calculadora binaria
 
 def son_todos_digitos(numero: str) -> bool: 
-    # Verifica que el numero pasado como argumento, contiene solo digitos. 
-    # Si es asì devuelve True, caso contrario False
-    if numero.isdigit():
-        return True
-    else:
-        return False
+    """
+    Verifica si el número pasado como argumento contiene solo dígitos.
 
-def son_todos_digitos(numero: str) -> bool: 
-    # Verifica que el numero pasado como argumento, contiene solo digitos. 
-    # Si es asì devuelve True, caso contrario False
-    if numero.isdigit():
-        return True
-    else:
-        return False
+    Args:
+        numero (str): Cadena a verificar.
 
-def tiene_longitud(numero: str) -> bool: 
-    if len(numero) != 0:
-        return True
-    else:
-        return False
+    Returns:
+        bool: True si la cadena contiene solo dígitos, False en caso contrario.
+    """
+    return numero.isdigit()
 
-def validar_numero_binario(mensaje: str):
-    num: str = input(f"{mensaje}: ")
-    # validar que ingrese algun valor
-    longitud: bool = tiene_longitud(num)
-    # validar que sean todos digitos
-    binario: bool = son_todos_digitos(num)
+def tiene_longitud(numero: str) -> bool:
+    """
+    Verifica que el número pasado como argumento tiene longitud.
 
-    if binario and longitud:
-        for i in range(0, len(num)):
-            if int(num[i]) < 0 or int(num[i]) > 1:
-                return validar_numero_binario("El numero ingresado no es binario, ingrese otro")
-    else:
+    Args:
+        numero (str): Cadena a verificar.
+
+    Returns:
+        bool: True si la cadena tiene al menos un carácter, False si está vacía.
+    """
+    return len(numero) != 0
+
+def es_binario(numero: str) -> bool:
+    """
+    Verifica si el número pasado como argumento es un número binario.
+
+    Args:
+        numero (str): Cadena a verificar.
+
+    Returns:
+        bool: True si la cadena es un número binario, False en caso contrario.
+    """
+    for i in range(0, len(numero)):
+        if int(numero[i]) < 0 or int(numero[i]) > 1:
+            return False
+    return True
+
+def validar_numero_binario(mensaje: str) -> str:
+    """
+    Solicita al usuario un número binario y válida:
+    * que la cadena no esté vacía.
+    * que la cadena contenga solo digitos.
+    * que la cadena contenga números 0 y 1
+
+    Args:
+        mensaje (str): Mensaje que se muestra al usuario para solicitar el número.
+
+    Returns:
+        str: El número binario válido ingresado por el usuario.
+    """
+    num: str = input(mensaje)
+
+    if not (tiene_longitud(num) and son_todos_digitos(num)):
         return validar_numero_binario("El numero ingresado no es valido, ingrese otro")
+
+    if not (es_binario(num)):
+        return validar_numero_binario("El numero ingresado no es binario, ingrese otro")
 
     return num
 
-def convertir_a_decimal(num1):
-    suma1 = 0
+def convertir_a_decimal(numero: str) -> int:
+    """
+    Convierte una cadena binaria a su valor decimal.
 
-    for i in range(len(num1), 0, -1):
-        suma1 += int(num1[i-1]) * (2 ** (len(num1)-i))
-    return suma1
+    Args:
+        numero (str): Cadena que representa un número binario.
 
-def agregar_numeros(num: str, longitud: int) -> str:
-    numero_invertido = num[::-1]
+    Returns:
+        int: Valor decimal equivalente.
+    """
+    decimal: int = 0
+
+    for i in range(len(numero), 0, -1):
+        decimal += int(numero[i-1]) * (2 ** (len(numero)-i))
+    return decimal
+
+def rellenar_con_ceros_izquierda(numero: str, longitud: int) -> str:
+    """
+    Rellena con ceros a la izquierda para igualar la longitud de los números binarios.
+
+    Args:
+        numero (str): Número binario como cadena.
+        longitud (int): Cantidad de ceros a agregar.
+
+    Returns:
+        str: Número binario con ceros agregados a la izquierda.
+    """
+    numero_invertido = numero[::-1]
     numero_invertido += longitud * "0"
     return numero_invertido[::-1]
 
 def sumar(numeros: list[str]):
-    num1: str = numeros[0]
-    num2: str = numeros[1]
-    total: str = ""
-    carry: int = 0
-    # Comprobar la longitud
-    longitud = abs(len(num1) - len(num2))
-    if len(num1) > len(num2):
-        num2 = agregar_numeros(num2, longitud)
-    else:
-        num1 = agregar_numeros(num1, longitud)
+    """
+    Suma dos números binarios representados como cadenas.
 
-    def sumando(num1: str, num2: str) -> str:
+    Args:
+        numeros (list[str]): Lista con dos cadenas que representan números binarios.
+
+    Returns:
+        str: Resultado de la suma en binario.
+    """
+    binario1: str = numeros[0]
+    binario2: str = numeros[1]
+    resultado : str = ""
+    carry: int = 0
+
+    longitud = abs(len(binario1) - len(binario2))
+    if len(binario1) > len(binario2):
+        binario2 = rellenar_con_ceros_izquierda(binario2, longitud)
+    else:
+        binario1 = rellenar_con_ceros_izquierda(binario1, longitud)
+
+    def sumando(num1: str, num2: str) -> None:
+        """
+        Suma dos dígitos binarios (como caracteres) y actualiza el resultado y el carry globales.
+
+        Args:
+            num1 (str): Primer dígito binario a sumar ('0' o '1').
+            num2 (str): Segundo dígito binario a sumar ('0' o '1').
+
+        Returns:
+            None: Modifica las variables 'resultado' y 'carry' no locales.
+        """
         nonlocal carry
-        nonlocal total
+        nonlocal resultado 
 
         if int(num1) == 0 and int(num2) == 0:
             carry -= 1
-            total += "0"
+            resultado  += "0"
         elif (int(num1) == 0 and int(num2) == 1) or (int(num1) == 1 and int(num2) == 0):
             carry -= 1
-            total += "1"
+            resultado  += "1"
         else:
             carry += 1
-            total += "0"
+            resultado  += "0"
 
     def sumando_con_carry(num1: str, num2: str) -> str:
+        """
+        Suma dos dígitos binarios considerando el acarreo previo y devuelve el resultado como carácter.
+
+        Args:
+            num1 (str): Primer dígito binario a sumar ('0' o '1').
+            num2 (str): Segundo dígito binario a sumar ('0' o '1').
+
+        Returns:
+            str: Resultado de la suma considerando el acarreo ('0' o '1').
+        """
         nonlocal carry
 
         if (int(num1) == 0 and int(num2) == 1) or (int(num1) == 1 and int(num2) == 0):
@@ -86,47 +158,50 @@ def sumar(numeros: list[str]):
             carry += 1
             return "0"
 
-    # igualar los digitos agregando 0 a la izquierda
-    # iterar y sumar cada digito empezando de der a izq
-    for i in range(len(num1)-1, -1, -1):
-        # carry = 0 if carry < 0 else carry
+    for i in range(len(binario1)-1, -1, -1):
+        # Reiniciamos el carry en caso negativo
         if carry < 0:
             carry = 0
 
         if carry > 0:
-            sumando(sumando_con_carry(num1[i], "1"), num2[i])
+            sumando(sumando_con_carry(binario1[i], "1"), binario2[i])
         else:
-            sumando(num1[i], num2[i])
+            sumando(binario1[i], binario2[i])
 
+    # Añadimos el carry final si es 1
     if carry > 0:
-        total += "1"
+        resultado += "1"
 
-    # agregar comparacion parceandolo a decimal
-    # escribir mejor los nombres
-    num1_parceado = convertir_a_decimal(num1)
-    num2_parceado = convertir_a_decimal(num2)
-    total_parceado = num1_parceado + num2_parceado
-    total2 = convertir_a_decimal(total[::-1])
-    if total_parceado == total2:
-        print("Esta bieeeee")
+    # Convertimos los binarios a decimales para verificar el resultado
+    decimal1 = convertir_a_decimal(binario1)
+    decimal2 = convertir_a_decimal(binario2)
+    total = decimal1 + decimal2
+    resultado_decimal = convertir_a_decimal(resultado[::-1])
+    if total == resultado_decimal:
+        print("Binario verificado...")
 
-    print(total)
-    print(total[::-1])
-    return total[::-1]
+    return resultado[::-1]
+
 # def resta():
 # def multiplicar():
 # def dividir():
 
 def calculadora():
+    """
+    Menú principal de la calculadora binaria.
+    Permite seleccionar la operación y solicita los números binarios al usuario.
+    """
     print("Elije la operacion a realizar del siguiente menu: ")
     print("1 - Sumar \n2 - Resta \n3 - Multiplicar \n4 - Dividir")
-    # Agregar la operacion para cerrar el programa
     operacion: int = int(input("Elije la operacion: "))
-    # Validar la operacion
     lista_de_operadores: tuple = (1, 2, 3, 4)
+    if operacion not in lista_de_operadores:
+        print("Opcion no valida, intente de nuevo")
+        return
+
     numeros: list = []
     for i in range(0, 2):
-        numeros.append(validar_numero_binario(f"Ingrese el numero binario {i+1}"))
+        numeros.append(validar_numero_binario(f"Ingrese el numero binario {i+1}: "))
 
     if operacion == lista_de_operadores[0]:
         print(f"El resultado es: {sumar(numeros)}")
@@ -136,12 +211,6 @@ def calculadora():
         print("Multiplicando")
     elif operacion == lista_de_operadores[3]:
         print("Dividiendo")
-
-# operador = 1
-# ["1010", "1"]
-# Validamos los arg.
-# Guardamos los arg.
-# Realizamos la operacion pertinente
 
 calculadora()
 
